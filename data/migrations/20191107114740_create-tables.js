9122b4713b8f48d28963a4536e83bc6c2fea7c4d
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-11-07 11:56:46 +0100

    wip

--- data/migrations/20191107114740_create-tables.js
+++ data/migrations/20191107114740_create-tables.js
@@ -1,11 +1,23 @@
 
 exports.up = function (knex) {
-  return knex.schema.createTable('species', table => {
-    table.increments()
-    table.string('species_name', 128)
-  })
+  return knex.schema
+    .createTable('species', table => {
+      table.increments()
+      table.string('species_name', 128)
+    })
+    .createTable('animals', table => {
+      table.increments()
+      table.string('animal_name', 128)
+      table.integer('species_id')
+        // we need positive integers
+        .unsigned()
+        // it should not be nullable
+        .notNullable()
+        // it needs to reference the id on the table
+        .references('id')
+        .inTable('species')
+    })
 };
 
 exports.down = function (knex) {
-
 };
